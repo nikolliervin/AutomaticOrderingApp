158984a3cef5eee7618d9e8c169172f08d1e7c78
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-09-19 17:39:56 +0200

    Created the dictionary of products

---
+++ AutomaticOrderingApp/Products.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace AutomaticOrderingApp
+{
+    public class Products
+    {
+        Dictionary<string, int> products = new Dictionary<string, int>
+        {
+            {"Barilla farfalle makarona",4 },
+            {"Solei vaj luledielli",1 },
+            {"coca cola 1.5",5 },
+            {"kazazi sallam",3 },
+            {"Qumesht",2 },
+            {"barilla nascondini",1 },
+            {"barilla salce",4 },
+            {"bravo",2 },
+            {"reas paketim",1 },
+            {"proshute krudo",1 },
+            {"kackavall i grire",3 },
+        };
+    }
+}
