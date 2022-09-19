fc12cc24048421aa4355891c94580acd15ac138d
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-09-19 16:50:16 +0200

    Added all products to config file

--- AutomaticOrderingApp/Steps.cs
+++ AutomaticOrderingApp/Steps.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace AutomaticOrderingApp
 {
     internal class Steps
     {
+        IWebDriver driver = new ChromeDriver();
+
     }
 }
